5b46d74c8d9a83a57feeb16a7ab6e9514e474d29
Slashgarcia <aicragh@outlook.com>
2021-01-27 15:24:47 -0600

    Project model modified

--- lib/models/ProjectModel.dart
+++ lib/models/ProjectModel.dart
@@ -1,3 +1,35 @@
+import 'dart:convert';
+import 'package:task_app/models/TaskModel.dart';
+
+ProjectModel projectModelFromJson(String str) =>
+    ProjectModel.fromJson(json.decode(str));
+
+String projectModelToJson(ProjectModel data) => json.encode(data.toJson());
+
+class ProjectModel {
+  ProjectModel({
+    this.ok,
+    this.project,
+    this.task,
+  });
+
+  bool ok;
+  Project project;
+  List<Task> task;
+
+  factory ProjectModel.fromJson(Map<String, dynamic> json) => ProjectModel(
+        ok: json["ok"],
+        project: Project.fromJson(json["project"]),
+        task: List<Task>.from(json["task"].map((x) => Task.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "ok": ok,
+        "project": project.toJson(),
+        "task": List<dynamic>.from(task.map((x) => x.toJson())),
+      };
+}
+
 class Project {
   Project({
     this.id,
